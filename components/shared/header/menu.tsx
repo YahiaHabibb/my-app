4082f75bb3d31dd61df353f2aca1be7566a7f68b
YahiaHabibb <yahiahabibtech@gmail.com>
2025-02-17 18:31:22 +0200

    13CreateCartSlider

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,24 +1,28 @@
-import { ShoppingCartIcon, } from 'lucide-react'
-import Link from 'next/link'
+import { EllipsisVertical } from 'lucide-react'
 import CartButton from './cart-button'
 
 
-export default function Menu() {
+
+const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
+
     return (
         <div className='flex justify-end'>
-            <nav className='flex gap-3 w-full'>
-                <CartButton />
-
-                <Link href='/cart' className='header-button'>
-                    <div className='flex items-end'>
-                    <ShoppingCartIcon className='h-6 w-6' />
-                    </div>
-                </Link>
-                <Link href='/signin' className='flex items-center header-button'>
-                Hello, Sign in
-                </Link>
-
+            <nav className='md:flex gap-3 hidden w-full'>
+        
+                {forAdmin ? null : <CartButton />}
             </nav>
-        </div>
-    )
+            <nav className='md:hidden'>
+
+            <EllipsisVertical className='h-6 w-6' />
+
+                <div className='flex items-center justify-between '>
+
+                </div>
+
+            <CartButton />
+        </nav>
+    </div>
+)
 }
+
+export default Menu
